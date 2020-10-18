38ce2aa1844b99cccca1cfe5b7c779ce5bca0b76
bingbing YAN <ybb.emily@gmail.com>
2020-10-18 23:25:57 +0200

    fetch links and render link list

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -13,26 +13,28 @@ const FEED_QUERY = gql`
         url
         description
       }
+      count
     }
   }
 `
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-        {
-          id: '1',
-          description: 'Prisma turns your database into a GraphQL API 😎',
-          url: 'https://www.prismagraphql.com',
-        },
-        {
-          id: '2',
-          description: 'The best GraphQL client',
-          url: 'https://www.apollographql.com/docs/react/',
-        },
-      ]
+   
     return (
     <Query query={FEED_QUERY}>
-        {() => linksToRender.map(link => <Link key={link.id} link={link} />)}
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>
+          if (error) return <div>Error</div>
+    
+          const linksToRender = data.feed.links
+          return (
+              
+            <div>
+                 We have {data.feed.count} links
+              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+            </div>
+          )
+        }}
     </Query>
   )
   }
